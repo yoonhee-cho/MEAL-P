e92ab0fcd9517acf57ea6c78e5434993b50fdc14
Yoon Hee Cho <yoonheecho1123@gmail.com>
2021-02-10 22:13:12 -0500

    feat: update item qty

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -9,8 +9,8 @@ export class Cart extends React.Component {
       items: []
     }
 
-    this.handleQuantity = this.handleQuantity.bind(this)
-    // this.handleRemove = this.handleRemove.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
   }
 
   async componentDidMount() {
@@ -27,63 +27,110 @@ export class Cart extends React.Component {
     })
   }
 
-  handleQuantity(itemId) {
-    console.log(itemId, 'hello???')
+  async handleChange(event, item) {
+    event.preventDefault()
+
+    let index = 0
+
+    this.state.items.map((el, idx) => {
+      if (el[0].groceryitemId === item.id) {
+        index = idx
+      }
+    })
+
+    const itemToUpdate = this.state.items[index]
+    const restItems = this.state.items.slice()
+    restItems.splice(index, 1)
+
+    itemToUpdate[0].quantity = Number(event.target.value)
+
+    restItems.push(itemToUpdate)
+    restItems.sort((a, b) => a.groceryitemId - b.groceryitemId)
+
+    this.setState({
+      items: restItems
+    })
+
+    const userId = this.props.user.id
+
+    await this.props.updateItem(userId, item)
+
+    await this.props.setCartItems(userId)
+
+    const quantityInCart = this.props.cart.map(item => {
+      return item.orderedItems
+    })
+
+    this.setState({
+      items: quantityInCart
+    })
   }
 
-  // handleRemove(item, event){
-  //     event.preventDefault()
-  //     const userId = this.props.user.id
-  //     this.props.deleteItem(userId, item)
-  // }
+  async handleRemove(event, item) {
+    event.preventDefault()
+    const userId = this.props.user.id
+    await this.props.deleteItem(userId, item)
+  }
 
   render() {
     const items = this.props.cart
+    console.log('this.props.cart', this.props.cart)
     return (
       <div>
         <div>
-          <h3>Grocery Shopping List</h3>
+          <div>
+            <h3>Grocery Shopping List</h3>
+          </div>
+
+          <div>
+            {items &&
+              items.map(item => {
+                return (
+                  <div key={item.id}>
+                    <div>{item.name}</div>
+                    <div>{item.price / 100}</div>
+
+                    <select
+                      id={item.id}
+                      label="Quantity"
+                      defaultValue={item.orderedItems[0].quantity}
+                      onChange={event => this.handleChange(event, item)}
+                    >
+                      {Array.from(Array(15)).map((el, idx) => {
+                        return (
+                          <option key={idx} value={idx + 1}>
+                            {idx + 1}
+                          </option>
+                        )
+                      })}
+                    </select>
+
+                    <div>subTotal: $ {item.orderedItems[0].subTotal / 100}</div>
+
+                    <div className="remove-button-container">
+                      <button
+                        type="button"
+                        className="remove-from-cart-button"
+                        onClick={event => this.handleRemove(event, item)}
+                      >
+                        Remove from Cart
+                      </button>
+                    </div>
+                  </div>
+                )
+              })}
+          </div>
         </div>
 
         <div>
-          {items &&
-            items.map(item => {
-              return (
-                <div key={item.id}>
-                  <div>{item.name}</div>
-
-                  <div>{item.price / 100}</div>
-
-                  <button
-                    type="button"
-                    className="adjust-qty"
-                    onClick={this.handleQuantity.bind(this, item.id)}
-                  >
-                    -
-                  </button>
-
-                  <button
-                    type="button"
-                    className="adjust-qty"
-                    onClick={this.handleQuantity.bind(this, item.id)}
-                  >
-                    +
-                  </button>
-
-                  <div>price:</div>
-
-                  <div className="remove-button-container">
-                    <button
-                      type="button"
-                      className="remove-from-cart-button"
-                      // onClick={this.handleRemove(item)}
-                    >
-                      Remove from Cart
-                    </button>
-                  </div>
-                </div>
-              )
-            })}
+          <h3>
+            TOTAL : ${this.props.cart
+              .reduce((accum, item) => {
+                accum += item.orderedItems[0].subTotal / 100
+                return accum
+              }, 0)
+              .toFixed(2)}
+          </h3>
         </div>
       </div>
     )
